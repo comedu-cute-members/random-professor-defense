74efc319537ae5f5cc924d6083291f967a9f6346
phenom0311 <joosang0311@gmail.com>
2022-10-30 03:44:22 +0900

    seonbaeScript get class info

--- My project/Assets/1. Scripts/SbScript.cs
+++ My project/Assets/1. Scripts/SbScript.cs
@@ -8,22 +8,35 @@ using UnityEngine.EventSystems;
 public class SbScript : MonoBehaviour
 {
     public int sbIndex;
-    DirectorScript directorScript_script;
 
-    float atk;
-    float spd;
-    float skill;
+    DirectorScript directorScript_script;
+    SbClass sbClass_script;
+
+    string name, skillName;
+    int atk, sklPower;
+    float spd, score;
+
     float distance = 10;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         sbIndex = UnityEngine.Random.Range(0, 29);
+
         directorScript_script = FindObjectOfType<DirectorScript>();
+        sbClass_script = GetComponent<SbClass>();
     }
 
-    // Update is called once per frame
+    void GetClassInfo()
+    {
+        name = sbClass_script.GetName();
+        score = sbClass_script.GetScore();
+        skillName = sbClass_script.GetSkillName();
+        atk = sbClass_script.GetAtkPower();
+        spd = sbClass_script.GetSpeed();
+        sklPower = sbClass_script.GetSklPower();
+
+    }
+
     void Update()
     {
         
@@ -32,10 +45,9 @@ public class SbScript : MonoBehaviour
     // Sb Drag
     void OnMouseDrag()
     {
-        Vector3 moustposition = new Vector3(Input.mousePosition.x, 
-            Input.mousePosition.y, distance);
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(moustposition);
-        transform.position = objPosition;
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 worldObjectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = worldObjectPosition;
     }
 
 }
